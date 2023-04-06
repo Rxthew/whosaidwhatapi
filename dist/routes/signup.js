df1262bf55c8ff22b7c5624e58de317a6245a16f
rxthew <richardtheumaster@gmail.com>
2023-04-06 23:55:20 +0200

    Set up route to use controller for signup

--- dist/routes/signup.js
+++ dist/routes/signup.js
@@ -4,8 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const signup_1 = __importDefault(require("../controllers/signup"));
 const router = express_1.default.Router();
-router.get('/', function (req, res, next) {
-    res.json({ test: 'tested' });
-});
+router.post('/', signup_1.default);
 exports.default = router;
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import {Request, Response, NextFunction} from 'express';
+import signUpController from '../controllers/signup';
 
 const router = express.Router();
 
-router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.json({test: 'tested'});
-});
+router.post('/', signUpController);
 
 export default router;
