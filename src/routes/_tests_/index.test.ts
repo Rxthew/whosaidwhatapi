4f9d62d696a9d308eb5b500ac3676f41aa7db3f8
rxthew <richardtheumaster@gmail.com>
2023-06-10 18:26:03 +0200

    Develop mockFind to check for user visibility.
    Add dummy user to request on isAuthenticated.
    
    Correct reference to Post.

--- src/routes/_tests_/index.test.ts
+++ src/routes/_tests_/index.test.ts
@@ -2,10 +2,9 @@ import {Request, Response as ExpressResponse, NextFunction} from 'express';
 import request from 'supertest';
 import { Response } from 'supertest'
 import app from '../../testapp';
-import Comment from '../../models/comment'
+import Post from '../../models/post';
 
-
-jest.mock('../../models/comment');
+jest.mock('../../models/post');
 
 
 const authTestSetup = function(){
@@ -14,10 +13,13 @@ const authTestSetup = function(){
     const toggleAuthTestVariable = function(){
         const newStatus = _authTestVariable.authenticated ? _authTestVariable.authenticated = false : _authTestVariable.authenticated = true;
         return _authTestVariable.authenticated
-    }
+    };
+
+
     const isAuthenticated = function(req:Request, res: ExpressResponse, next:NextFunction){
         const isAuth = function(){return _authTestVariable.authenticated};
         Object.assign(req, {isAuthenticated: isAuth});
+        Object.assign(req, { user: {username: 'Jane Doe', member_status: 'regular'} })
         next();
     };
 
@@ -33,7 +35,15 @@ const {isAuthenticated, toggleAuthTestVariable} = authTestSetup();
 app.use(isAuthenticated);
 
 const generateMocks = function(){
-    const mockFind = jest.fn();
+    const mockFind = jest.fn(() => {
+        return {
+            populate: (obj: Record<string, any>) => {
+                return { 
+                    exec: () => obj.select.user
+                }
+            },
+        }
+    })
 
     return {
         mockFind
@@ -41,5 +51,8 @@ const generateMocks = function(){
 };
 
 const { mockFind } = generateMocks();
-Comment.find = mockFind;
+
+(Post as Record<'find',any>).find = mockFind;
+
+
 
