3e993d76b11030b76b97b0f8dce6e2bb8e7d0857
rxthew <richardtheumaster@gmail.com>
2023-06-12 19:15:54 +0200

    First version of comment controller.
    Applicable for comment creation only for now:
    
    Ensure user authenticated.
    
    Include validation of objectIds.
    
    Implement createComment.

---
+++ src/controllers/comment.ts
@@ -0,0 +1,127 @@
+import { Request, Response, NextFunction } from 'express';
+import { body } from 'express-validator';
+import mongoose, { ObjectId } from 'mongoose';
+import  Comment  from '../models/comment';
+import Post from '../models/post';
+import { User } from '../models/user';
+import { basicValidation, redirectPage } from './helpers/services';
+
+const Types = mongoose.Types;
+
+
+const _checkUserIsAuthenticated = function(req:Request, res:Response, next:NextFunction){
+    if(req.isAuthenticated()){
+        next()
+    }
+    else{
+        res.json({'error': 'User is not authenticated'})
+
+    }  
+};
+
+const _checkValidityOfPostId = function(id:string | ObjectId | unknown){
+    const result = mongoose.isObjectIdOrHexString(id);
+    if(!result){
+        throw new Error('Post\'s object id is invalid.')
+    }
+    return result    
+
+};
+
+const _checkValidityOfUserId = function(id:string | ObjectId | unknown){
+    const result = mongoose.isObjectIdOrHexString(id);
+    if(!result){
+        throw new Error('User\'s object id is invalid.')
+    }
+    return result
+
+};
+
+const _generateDate = function(){  //To refine
+    return Date.now(); 
+};
+
+const _postExistsInDatabase = async function(id:string | ObjectId | unknown){
+    const result = await Post.exists({'_id': id}).catch((err:Error)=>{throw err});
+    if(!result){
+        throw new Error('Post object id is not in database')
+    }
+    return result
+
+
+};
+
+const _userExistsInDatabase = async function(id:string | ObjectId | unknown){
+    const result = await User.exists({'_id': id}).catch((err:Error)=>{throw err});
+    if(!result){
+        throw new Error('User object id is not in database')
+    }
+    return result
+
+};
+
+
+const commentValidation = basicValidation
+
+const confirmCommentCreated = function(req:Request, res:Response, next: NextFunction){
+    res.json({status: 'Comment created successfully.' })
+}
+
+
+
+const createComment = async function(req:Request, res:Response, next:NextFunction){
+    const db = mongoose.connection;
+    try{
+        await db.transaction(async function finaliseCommentCreate(session){
+            await Comment.create([{
+                content: req.body.content,
+                post: new Types.ObjectId(req.body.post.trim()), 
+                user: new Types.ObjectId(req.body.user.trim()), 
+                date: _generateDate()}], {session})
+            .catch(
+                (err:Error)=> {throw err}
+                )
+
+        }).catch(
+            (err:Error)=> {throw err}
+            )
+    }catch(err){
+        console.log(err)
+        res.json({err: err})
+    }
+    next()
+};
+
+
+const postCommentController = [
+    _checkUserIsAuthenticated,
+    body('content','Content must not be empty.')
+    .trim()
+    .escape(),
+    body('post','Post id must not be empty.')
+    .trim()
+    .escape(),
+    body('user','User id must not be empty.')
+    .trim()
+    .escape(),
+    body('post').custom(_checkValidityOfPostId),
+    body('user').custom(_checkValidityOfUserId),
+    body('post').custom(_postExistsInDatabase),
+    body('user').custom(_userExistsInDatabase),
+    commentValidation,
+    createComment,
+    redirectPage,
+    confirmCommentCreated
+];
+
+const putCommentController = [
+    body('content','Content must not be empty.')
+    .trim()
+    .escape()
+
+];
+
+const deleteCommentController = [
+
+];
+
