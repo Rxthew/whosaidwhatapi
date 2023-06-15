72f72d606c1543beec9f8c4d734044e767a3fe5d
rxthew <richardtheumaster@gmail.com>
2023-06-15 12:28:56 +0200

    Implement put controller for comment.
    Add updateComment and custom validators.
    
    Add putCommentController to PUT route.

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -19,6 +19,15 @@ const _checkUserIsAuthenticated = function(req:Request, res:Response, next:NextF
     }  
 };
 
+const _checkValidityOfCommentId = function(id:string | mongoose.Types.ObjectId | unknown){
+    const result = mongoose.isObjectIdOrHexString(id);
+    if(!result){
+        throw new Error('Comment\'s object id is invalid.')
+    }
+    return result
+
+};
+
 const _checkValidityOfPostId = function(id:string | mongoose.Types.ObjectId | unknown){
     const result = mongoose.isObjectIdOrHexString(id);
     if(!result){
@@ -41,6 +50,15 @@ const _generateDate = function(){  //To refine
     return Date.now(); 
 };
 
+
+const _commentExistsInDatabase = async function(id:string | mongoose.Types.ObjectId | unknown){
+    const result = await Comment.exists({'_id': id}).catch((err:Error)=>{throw err});
+    if(!result){
+        throw new Error('Comment object id is not in database')
+    }
+    return result
+};
+
 const _postExistsInDatabase = async function(id:string | mongoose.Types.ObjectId | unknown){
     const result = await Post.exists({'_id': id}).catch((err:Error)=>{throw err});
     if(!result){
@@ -61,19 +79,48 @@ const _userExistsInDatabase = async function(id:string | mongoose.Types.ObjectId
 };
 
 
-const commentValidation = basicValidation
+const commentValidation = basicValidation;
 
 const confirmCommentCreated = function(req:Request, res:Response, next: NextFunction){
     res.json({status: 'Comment created successfully.' })
-}
+};
 
 
+const confirmCommentUpdated = function(req:Request, res:Response, next: NextFunction){
+    res.json({status: 'Comment updated successfully.' })
+};
 
 const createComment = async function(req:Request, res:Response, next:NextFunction){
     const db = mongoose.connection;
     try{
         await db.transaction(async function finaliseCommentCreate(session){
             await Comment.create([{
+                content: req.body.content,
+                post: new Types.ObjectId(req.body.post.trim()), 
+                user: new Types.ObjectId(req.body.user.trim()), 
+                date: _generateDate()}], {session})
+            .catch(
+                (err:Error)=> {throw err}
+                )
+
+        }).catch(
+            (err:Error)=> {throw err}
+            )
+    }catch(err){
+        console.log(err)
+        res.json({err: err})
+    }
+    next()
+};
+
+const updateComment = async function(req:Request, res:Response, next:NextFunction){
+    const db = mongoose.connection;
+    try{
+        await db.transaction(async function finaliseCommentUpdate(session){
+            await Comment.updateOne({
+                _id: req.body._id
+                },
+                [{
                 content: req.body.content,
                 post: new Types.ObjectId(req.body.post.trim()), 
                 user: new Types.ObjectId(req.body.user.trim()), 
@@ -120,11 +167,38 @@ export const postCommentController = [
     confirmCommentCreated
 ];
 
-const putCommentController = [
+export const putCommentController = [
+    _checkUserIsAuthenticated,
     body('content','Content must not be empty.')
     .trim()
-    .escape()
-
+    .notEmpty()
+    .withMessage('Content must not be empty.')
+    .escape(),
+    body('_id','Comment id must not be empty.')
+    .trim()
+    .notEmpty()
+    .withMessage('Comment id must not be empty.')
+    .escape(),
+    body('post','Post id must not be empty.')
+    .trim()
+    .notEmpty()
+    .withMessage('Post id must not be empty.')
+    .escape(),
+    body('user','User id must not be empty.')
+    .trim()
+    .notEmpty()
+    .withMessage('User id must not be empty.')
+    .escape(),
+    body('_id').custom(_checkValidityOfCommentId),
+    body('post').custom(_checkValidityOfPostId),
+    body('user').custom(_checkValidityOfUserId),
+    body('_id').custom(_commentExistsInDatabase),
+    body('post').custom(_postExistsInDatabase),
+    body('user').custom(_userExistsInDatabase),
+    commentValidation,
+    updateComment,
+    redirectPage,
+    confirmCommentUpdated
 ];
 
 const deleteCommentController = [
--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { postCommentController } from '../controllers/comment';
+import { postCommentController, putCommentController } from '../controllers/comment';
 
 const router = express.Router();
 
 router.post('/', postCommentController);
+router.put('/', putCommentController);
 
 export default router;
 
