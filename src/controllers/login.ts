59714713841e0164323d81e5ceebf0072ba9613d
rxthew <richardtheumaster@gmail.com>
2023-06-19 22:00:33 +0200

    Refactor login controller.
    Referer field is an option only, not a requirement.
    
    Implement confirm login option.
    
    Use service for validation.

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,58 +1,32 @@
 import { Request, Response, NextFunction } from 'express';
-import { body, header, Result, ValidationError, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import passport from 'passport';
+import { basicValidation, redirectToOrigin, redirectToReferringPage } from './helpers/services';
 
 
-const _loginValidationFailed = function(res:Response, errors: Result<ValidationError>){
-    const passedErrors = {errors: errors.mapped()}
-    res.status(400).json(passedErrors);
-    return
-};
 
-
-const _supplyReferer = function(req:Request,res:Response,next:NextFunction){
+const authenticateUser = function(req:Request, res:Response, next:NextFunction){
     const referer = req.get('Referer')
-    if(referer){
-        return referer
-    }
-    else{
-        res.json({errors: {
-            msg: 'Referer header is not set. Sign-up complete. Please go back to homepage.'
-        }})
-    }
+    return referer ? passport.authenticate('local', {failureRedirect: referer, failureMessage: true})(req,res,next) : passport.authenticate('local')(req,res,next)
+    
 };
 
-const _supplyUserInfo = function(req:Request, res:Response, next:NextFunction){
-    const referer = _supplyReferer(req,res,next);
+const confirmLogin = function(req:Request, res:Response, next: NextFunction){
+    res.json({status: 'Login successful.' })
+};
+
+const loginValidation = basicValidation;
+
+const supplyUserInfo = function(req:Request, res:Response, next:NextFunction){
     if(req.user){
         const user = Object.assign(req.user, {password: ''});
         res.json(user)
     }
-    else if(referer){
-        res.redirect(referer)
-    }
+    next()
 };
 
-const authenticateUser = function(req:Request, res:Response, next:NextFunction){
-    const redirect = _supplyReferer(req,res,next);
-    return passport.authenticate('local', {failureRedirect: redirect, failureMessage: true}, _supplyUserInfo)(req,res,next)
-    
-};
-
-const loginValidation = function(req:Request,res:Response,next:NextFunction){
-    const errors = validationResult(req);
-    const checkEmpty = errors.isEmpty();
-    if(checkEmpty){
-        next()
-    }
-    else{
-        _loginValidationFailed(res,errors)
-    }
-};
 
 const loginController = [
-    header('Referer').exists()
-    .withMessage('Referer header must not be empty.'),
     body('username', 'username must not be empty')
     .trim()
     .escape(),
@@ -62,7 +36,11 @@ const loginController = [
     .withMessage('password needs to be a minimum of 8 characters')
     .escape(),
     loginValidation,
-    authenticateUser
+    authenticateUser,
+    supplyUserInfo,
+    redirectToReferringPage,
+    redirectToOrigin, 
+    confirmLogin
 ];
 
 export default loginController
