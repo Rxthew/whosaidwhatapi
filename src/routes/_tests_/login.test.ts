459a86aceec6b417d9c06bc3e924287228cfb61a
rxthew <richardtheumaster@gmail.com>
2023-06-19 22:00:54 +0200

    Remove redirect to referer test for login.

--- src/routes/_tests_/login.test.ts
+++ src/routes/_tests_/login.test.ts
@@ -1,7 +1,6 @@
 
-import {Request, Response as expressRes, NextFunction} from 'express';
+import {Request, Response, NextFunction} from 'express';
 import request from 'supertest';
-import { Response } from 'supertest'
 import app from '../../testapp';
 import passport from 'passport';
 
@@ -9,45 +8,20 @@ jest.mock("passport", () => ({
     __esModule: true,
     default: {
         authenticate: () => {
-            return (req:Request, res:expressRes, next:NextFunction) => {
+            return (req:Request, res:Response, next:NextFunction) => {
                 res.json({test: 'authenticated'})
             }
         }
     }
 }));
 
-const addOns = function(){
-    
-    const checkIfErrorsPresent = function(res:Response){
-        if(!('errors' in res.body)){throw new Error('Errors object not present')}
-    };
-
-    const origin = 'http://localhost:3000';
-    return {
-        checkIfErrorsPresent,
-        origin,
-    }
-
-};
-
 
 describe('Login should call authenticate when passing validation, else it should redirect or return errors.', ()=>{
-
-    const { checkIfErrorsPresent, origin } = addOns();
-
-    it('Request supplied without Referer header field should throw an error', (done) =>{
-        request(app)
-        .post('/login')
-        .set('Accept','application/json')
-        .send({username: 'janedoe', password: 'janedoe1'})
-        .expect(checkIfErrorsPresent)
-        .expect(400,done)
-    });
 
     it('Valid credentials should call authentication', (done) => {
         request(app)
         .post('/login')
-        .set('Referer', origin)
+        .set('Origin', 'http://localhost:3000')
         .set('Accept','application/json')
         .send({username: 'janedoe', password: 'janedoe1'})
         .expect(200)
