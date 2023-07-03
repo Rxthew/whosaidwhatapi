e0d9f722e89da5271fd3e54fa984745f7a28a3d6
rxthew <richardtheumaster@gmail.com>
2023-07-03 13:35:55 +0200

    Add nullifySessionId to log out controller.
    See comment therein and issue 471 of connect-mongo repo for context.

--- src/controllers/logout.ts
+++ src/controllers/logout.ts
@@ -1,6 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 import { redirectToReferringPage, redirectToOrigin } from './helpers/services';
 
+
+//Note: reason I am nullifying the sessionID is because of race conditions in express
+//session where the touch method is sometimes invoked after the destroy method thereby
+//producing an error. Nullifying the sessionID prevents the touch method from being
+//invoked in the first place.
+
+const nullifySessionId = function(req:Request, res:Response, next:NextFunction){
+    req.sessionID ? Object.assign(req, {sessionID: null}) : false
+    next()
+}
+
 const logout = function(req:Request,res:Response,next:NextFunction){
     req.logout(function(err){
         if(err){
@@ -12,6 +23,7 @@ const logout = function(req:Request,res:Response,next:NextFunction){
 };
 
 export const logoutController = [
+    nullifySessionId,
     logout,
     redirectToReferringPage,
     redirectToOrigin
