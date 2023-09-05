cd81802c038ea352885e2cd94012cab54c2fc0e4
rxthew <richardtheumaster@gmail.com>
2023-09-05 21:37:42 +0200

    Explicitly set same site to 'none'.

--- dist/app.js
+++ dist/app.js
@@ -56,7 +56,7 @@ app.use(
     cookie: {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24,
-      sameSite: false,
+      sameSite: 'none',
       secure: process.env.NODE_ENV === "production" ? true : false,
     }
   })
--- src/app.ts
+++ src/app.ts
@@ -53,7 +53,7 @@ app.use(
     cookie: {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24,
-      sameSite: false,
+      sameSite: 'none',
       secure: process.env.NODE_ENV === "production" ? true : false,
     }
   })
